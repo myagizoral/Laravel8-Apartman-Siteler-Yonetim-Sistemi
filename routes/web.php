6ba5a0fe2a70a50087e59c1ad23043aaf97d0e6d
Mustafa Yağız ORAL <mustafa.oral@outlook.com.tr>
2021-01-03 22:01:43 +0300

    Contentlere image gallery eklendi
    Contentlere image gallery eklendi

--- routes/web.php
+++ routes/web.php
@@ -37,15 +37,38 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-//admin
+//admin menu
 Route::middleware('auth')->prefix('admin')->group(function (){
 
     Route::get( '/',[\App\Http\Controllers\Admin\HomeController::class,'index'])->name('admin_home');
 
     Route::get( '/menu',[\App\Http\Controllers\Admin\MenuController::class,'index'])->name('admin_menu');
     Route::get( '/menu/add',[\App\Http\Controllers\Admin\MenuController::class,'add'])->name('admin_menu_add');
+    Route::get( '/menu/edit/{id}',[\App\Http\Controllers\Admin\MenuController::class,'edit'])->name('admin_menu_edit');
     Route::post( '/menu/create',[\App\Http\Controllers\Admin\MenuController::class,'create'])->name('admin_menu_create');
-    Route::post( '/menu/update',[\App\Http\Controllers\Admin\MenuController::class,'update'])->name('admin_menu_update');
+    Route::post( '/menu/update/{id}',[\App\Http\Controllers\Admin\MenuController::class,'update'])->name('admin_menu_update');
     Route::get( '/menu/delete/{id}',[\App\Http\Controllers\Admin\MenuController::class,'destroy'])->name('admin_menu_delete');
     Route::get( '/menu/show',[\App\Http\Controllers\Admin\MenuController::class,'show'])->name('admin_menu_show');
+
+    //contents
+    Route::prefix('content')->group(function() {
+
+        Route::get( '/',[\App\Http\Controllers\Admin\ContentController::class,'index'])->name('admin_contents');
+        Route::get( 'create',[\App\Http\Controllers\Admin\ContentController::class,'create'])->name('admin_content_add');
+        Route::post( 'store',[\App\Http\Controllers\Admin\ContentController::class,'store'])->name('admin_content_store');
+        Route::get( 'edit/{id}',[\App\Http\Controllers\Admin\ContentController::class,'edit'])->name('admin_content_edit');
+        Route::post( 'update/{id}',[\App\Http\Controllers\Admin\ContentController::class,'update'])->name('admin_content_update');
+        Route::get( 'delete/{id}',[\App\Http\Controllers\Admin\ContentController::class,'destroy'])->name('admin_content_delete');
+        Route::get( 'show',[\App\Http\Controllers\Admin\ContentController::class,'show'])->name('admin_content_show');
+    });
+
+//image gallery
+    Route::prefix('image')->group(function() {
+        Route::get( 'create/{content_id}',[\App\Http\Controllers\Admin\ImageController::class,'create'])->name('admin_image_add');
+        Route::post( 'store/{content_id}',[\App\Http\Controllers\Admin\ImageController::class,'store'])->name('admin_image_store');
+        Route::get( 'delete/{id}/{content_id}',[\App\Http\Controllers\Admin\ImageController::class,'destroy'])->name('admin_image_delete');
+        Route::get( 'show',[\App\Http\Controllers\Admin\ImageController::class,'show'])->name('admin_image_show');
+    });
 });
+
+
